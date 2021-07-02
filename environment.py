5aea45ac14b0d73699a346c94eb89aaa924ef57d
JosselinTD <josselin.td@gmail.com>
2021-07-02 21:33:52 +0200

    [Environment]: add test and fix class

--- environment.py
+++ environment.py
@@ -1,9 +1,9 @@
 import math
+import re
 
-def Environment:
-  mapSize = 4
-  
-  def __init__(self):
+class Environment:
+  def __init__(self, mapSize):
+    self.mapSize = mapSize
     self.allActions = []
     for i in range(self.mapSize * self.mapSize):
       self.allActions.append('SP' + str(i))
@@ -17,7 +17,7 @@ def Environment:
     if not self.isActionLegal(state, action):
       raise AttributeError(f"Action {action} not allowed in state {state}")
     
-    index = extractIndex(action, 'W' in action)
+    index = self.extractIndex(action, 'W' in action)
     reward = 0
     if 'W' not in action:
       if 'SP' in action or 'TP' in action or 'EP' in action: reward += 1 # 1 point when adding a room
@@ -25,15 +25,14 @@ def Environment:
         spColumn = state.index('1') % self.mapSize
         spRow = int(math.ceil(float(state.index('1')) / self.mapSize))
         actionColumn = index % self.mapSize
-        actionRow = int(math.ceil(float(index) / self.mapSize))
+        actionRow = int(float(index) / self.mapSize)
         reward += abs(spColumn - actionColumn) + abs(spRow - actionRow) # +1 point for each case between starting room and other room
     else:
       if '1' in state and ('2' in state or '3' in state):
-        previouslyAdjacentSpaces = self.listAllAdjacentSpaces(state, state.index('1'))
+        previouslyAdjacentSpaces = self.listAllAdjacentSpaces(state, state.index('1') + 1)
         newState = self.updateState(state, action)
-        newlyAdjacentSpaces = self.listAllAdjacentSpaces(newState, newStatestate.index('1'))
-        if ('2' in state and state.index('2') not in previouslyAdjacentSpaces and state.index('2') in newlyAdjacentSpaces) or
-           ('3' in state and state.index('3') not in previouslyAdjacentSpaces and state.index('3') in newlyAdjacentSpaces): reward += 5 # +5 point when connecting starting room to another room
+        newlyAdjacentSpaces = self.listAllAdjacentSpaces(newState, newState.index('1') + 1)
+        if ('2' in state and state.index('2') + 1 not in previouslyAdjacentSpaces and state.index('2') + 1 in newlyAdjacentSpaces) or ('3' in state and state.index('3') + 1 not in previouslyAdjacentSpaces and state.index('3') + 1 in newlyAdjacentSpaces): reward += 5 # +5 point when connecting starting room to another room
     return reward
 
   def curatedActions(self, state):
@@ -43,7 +42,7 @@ def Environment:
     if not self.isActionLegal(state, action):
       raise AttributeError(f"Action {action} not allowed in state {state}")
 
-    index = extractIndex(action, 'W' in action)
+    index = self.extractIndex(action, 'W' in action)
     newValue = '0'
     if 'SP' in action: newValue = '1'
     if 'TP' in action: newValue = '2'
@@ -54,39 +53,34 @@ def Environment:
     return state
 
   def isActionLegal(self, state, action):
-    index = extractIndex(action, 'W' in action):
-
+    index = self.extractIndex(action, 'W' in action)
+    wallNumber = index - self.mapSize * self.mapSize
     # Index issues
-    if index < 0: return False
     if 'W' not in action and index > self.mapSize * self.mapSize: return False
-    if 'W' in action and index > (self.mapSize - 1) * self.mapSize * 2: return False
-
+    if 'W' in action and wallNumber > (self.mapSize - 1) * self.mapSize * 2: return False
     # Already existing rooms
     if '1' in state and 'SP' in action: return False
     if '2' in state and 'TP' in action: return False
     if '3' in state and 'EP' in action: return False
 
     # Already filled room
-    if 'W' not in action and state[index] != 0: return False
+    if 'W' not in action and state[index] != '0': return False
 
     # All non wall illegal action have been filtered when we are here
     if 'W' not in action: return True
 
     # Already breaked walls
-    if state[index] == 0: return False
-
+    if state[index] == '0': return False
     # 2x2 empty room
-    wallNumber = index - self.mapSize * self.mapSize
     isAVerticalWall = self.isAVerticalWall(wallNumber)
-
     if isAVerticalWall:
       upperWallsToCheck = [
         # Upper wall
         index - (self.mapSize * 2 - 1),
         # Diagonal upper left
-        index - self.mapSize
+        index - self.mapSize,
         # Diagonal upper right
-        index - self.mapSize - 1
+        index - self.mapSize + 1
       ]
       lowerWallsToCheck = [
         # Lower wall
@@ -94,12 +88,12 @@ def Environment:
         # Diagonal lower right
         index + self.mapSize,
         # Diagonal lower left
-        index + self.mapSize +1
+        index + self.mapSize - 1
       ]
       if wallNumber > self.mapSize: # Check upper walls only if map exist there
         allDestroyed = True
         for i in upperWallsToCheck:
-          if state[i] == 1:
+          if state[i] == '4':
             allDestroyed = False
             break
         if allDestroyed: return False
@@ -107,7 +101,7 @@ def Environment:
       if wallNumber < ((self.mapSize - 1) * self.mapSize * 2) - (self.mapSize - 1): # Check lower walls only if map exist there
         allDestroyed = True
         for i in lowerWallsToCheck:
-          if state[i] == 1:
+          if state[i] == '4':
             allDestroyed = False
             break
         if allDestroyed: return False
@@ -116,7 +110,7 @@ def Environment:
         # Upper wall
         index - self.mapSize,
         # Left wall
-        index - 1
+        index - 1,
         # Lower wall
         index + self.mapSize - 1
       ]
@@ -124,14 +118,14 @@ def Environment:
         # Upper wall
         index - self.mapSize - 1,
         # Right wall
-        index + 1
+        index + 1,
         # Lower wall
         index + self.mapSize
       ]
       if wallNumber % (self.mapSize * 2 - 1) != self.mapSize: # Check left walls only if map exist there
         allDestroyed = True
         for i in leftWallsToCheck:
-          if state[i] == 1:
+          if state[i] == '4':
             allDestroyed = False
             break
         if allDestroyed: return False
@@ -139,7 +133,7 @@ def Environment:
       if wallNumber % (self.mapSize * 2 - 1) != 0: # Check right walls only if map exist there
         allDestroyed = True
         for i in rightWallsToCheck:
-          if state[i] == 1:
+          if state[i] == '4':
             allDestroyed = False
             break
         if allDestroyed: return False
@@ -148,8 +142,8 @@ def Environment:
 
   def extractIndex(self, action, isWall):
     shift = -1
-    if isWall : shift = self.mapSize * self.mapSize
-    return re.search(r'\d+', action).group() + shift
+    if isWall : shift = self.mapSize * self.mapSize - 1
+    return int(re.search(r'\d+', action).group()) + shift
 
   def listAllAdjacentSpaces(self, state, spaceIndex):
     spacesToAnalyze = [spaceIndex]
@@ -159,9 +153,9 @@ def Environment:
       adjacentSpaces.append(spaceToAnalyze)
       walls = self.getSpaceWalls(spaceToAnalyze)
       for wall in walls:
-        if state[wall] == 0:
+        if state[wall - 1] == '0': # getSpaceWalls return indexes starting on 1 and not 0
           spacesToAnalyze.extend(self.getWallSpaces(wall))
-      spacesToAnalyze = [space for space in spacesToAnalyze if space not in adjacentSpaces]
+      spacesToAnalyze = [space for space in spacesToAnalyze if space not in adjacentSpaces and space > 0 and space <= self.mapSize * self.mapSize]
     return adjacentSpaces
 
   def getSpaceWalls(self, spaceIndex):
@@ -171,30 +165,26 @@ def Environment:
     leftWallIndex = upperWallIndex + (self.mapSize - 1)
     rightWallIndex = leftWallIndex + 1
     lowerWallIndex = upperWallIndex + (self.mapSize * 2 - 1)
-
     walls = []
     if spaceIndex > self.mapSize : walls.append(upperWallIndex)
     if spaceIndex % self.mapSize != 1 : walls.append(leftWallIndex)
     if spaceIndex % self.mapSize != 0 : walls.append(rightWallIndex)
-    if spaceIndex < self.mapSize * self.mapSize - self.mapSize : walls.append(lowerWallIndex)
+    if spaceIndex <= self.mapSize * self.mapSize - self.mapSize : walls.append(lowerWallIndex)
 
     return walls
 
   def getWallSpaces(self, wallIndex):
     wallNumber = wallIndex - self.mapSize * self.mapSize
-    isAVerticalWall = self.isAVerticalWall(wallNumber)
-
-    if isAVerticalWall:
-      upperSpaceIndex = wallNumber - ((wallNumber / (self.mapSize * 2 - 1)) + 1)
+    isAVerticalWall = self.isAVerticalWall(wallNumber - 1)
+    if not isAVerticalWall:
+      upperSpaceIndex = wallNumber - (math.ceil(wallNumber / (self.mapSize * 2 - 1)) * 3)
       lowerSpaceIndex = upperSpaceIndex + self.mapSize
-
-      return [upperSpaceIndex, lowerSpaceIndex]
+      return [int(math.ceil(upperSpaceIndex)), int(math.ceil(lowerSpaceIndex))]
     else:
-      rightSpaceIndex = wallNumber - ((wallNumber / (self.mapSize * 2 - 1)) * 3)
+      rightSpaceIndex = wallNumber - (int(wallNumber / (self.mapSize * 2 - 1)) * 3)
       leftSpaceIndex = rightSpaceIndex + 1
 
-      return [rightSpaceIndex, leftSpaceIndex]
-
+      return [int(math.ceil(rightSpaceIndex)), int(math.ceil(leftSpaceIndex))]
 
   def isAVerticalWall(self, wallNumber):
     return (wallNumber % ((self.mapSize * 2) - 1)) < (self.mapSize - 1)
---
+++ test/test_environment.py
@@ -0,0 +1,143 @@
+from environment import Environment
+import unittest
+
+class TestEnvironmentClass(unittest.TestCase):
+  blankMap = '0000000000000000000000000000000000000000'
+
+  fullWallMap = '0000000000000000444444444444444444444444'
+
+  endMap = '1000000002000003000040444004040440404440'
+
+  spMap = '1000000000000000000000000000000000000000'
+
+  connectingMap = '1000002000000000004444444444444444444444'
+
+  def setUp(self):
+    self.environment = Environment(4)
+
+  def test_initialisation(self):
+    self.assertEqual(self.environment.mapSize, 4, 'incorrect map size')
+    self.assertEqual(len(self.environment.allActions), 16 * 3 + 24, 'incorrect all actions size')
+
+  def test_isAVerticalWall(self):
+    self.assertEqual(self.environment.isAVerticalWall(0), True)
+    self.assertEqual(self.environment.isAVerticalWall(1), True)
+    self.assertEqual(self.environment.isAVerticalWall(2), True)
+
+    self.assertEqual(self.environment.isAVerticalWall(3), False)
+    self.assertEqual(self.environment.isAVerticalWall(4), False)
+    self.assertEqual(self.environment.isAVerticalWall(5), False)
+    self.assertEqual(self.environment.isAVerticalWall(6), False)
+
+    self.assertEqual(self.environment.isAVerticalWall(7), True)
+    self.assertEqual(self.environment.isAVerticalWall(8), True)
+    self.assertEqual(self.environment.isAVerticalWall(9), True)
+
+    self.assertEqual(self.environment.isAVerticalWall(10), False)
+    self.assertEqual(self.environment.isAVerticalWall(11), False)
+    self.assertEqual(self.environment.isAVerticalWall(12), False)
+    self.assertEqual(self.environment.isAVerticalWall(13), False)
+
+    self.assertEqual(self.environment.isAVerticalWall(14), True)
+    self.assertEqual(self.environment.isAVerticalWall(15), True)
+    self.assertEqual(self.environment.isAVerticalWall(16), True)
+
+    self.assertEqual(self.environment.isAVerticalWall(17), False)
+    self.assertEqual(self.environment.isAVerticalWall(18), False)
+    self.assertEqual(self.environment.isAVerticalWall(19), False)
+    self.assertEqual(self.environment.isAVerticalWall(20), False)
+
+    self.assertEqual(self.environment.isAVerticalWall(21), True)
+    self.assertEqual(self.environment.isAVerticalWall(22), True)
+    self.assertEqual(self.environment.isAVerticalWall(23), True)
+  
+  def test_illegalAction(self):
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W200'), False)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'SP200'), False)
+
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'SP1'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'TP1'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'EP1'), True)
+    self.assertEqual(self.environment.isActionLegal(self.endMap, 'SP1'), False)
+    self.assertEqual(self.environment.isActionLegal(self.endMap, 'TP1'), False)
+    self.assertEqual(self.environment.isActionLegal(self.endMap, 'EP1'), False)
+
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W1'), False)
+    self.assertEqual(self.environment.isActionLegal(self.fullWallMap, 'W1'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W12'), False)
+    self.assertEqual(self.environment.isActionLegal(self.fullWallMap, 'W12'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W16'), False)
+    self.assertEqual(self.environment.isActionLegal(self.fullWallMap, 'W16'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W21'), False)
+    self.assertEqual(self.environment.isActionLegal(self.fullWallMap, 'W21'), True)
+    self.assertEqual(self.environment.isActionLegal(self.blankMap, 'W24'), False)
+    self.assertEqual(self.environment.isActionLegal(self.fullWallMap, 'W24'), True)
+
+  def test_getSpaceWalls(self):
+    self.assertEqual(self.environment.getSpaceWalls(6), [5+16,8+16,9+16,12+16])
+    self.assertEqual(self.environment.getSpaceWalls(1), [1+16,4+16])
+    self.assertEqual(self.environment.getSpaceWalls(12), [14+16,17+16,21+16])
+    self.assertEqual(self.environment.getSpaceWalls(16), [21+16,24+16])
+
+  def test_getWallSpaces(self):
+    self.assertEqual(self.environment.getWallSpaces(1+16), [1,2])
+    self.assertEqual(self.environment.getWallSpaces(2+16), [2,3])
+    self.assertEqual(self.environment.getWallSpaces(3+16), [3,4])
+
+    self.assertEqual(self.environment.getWallSpaces(4+16), [1,5])
+    self.assertEqual(self.environment.getWallSpaces(5+16), [2,6])
+    self.assertEqual(self.environment.getWallSpaces(6+16), [3,7])
+    self.assertEqual(self.environment.getWallSpaces(7+16), [4,8])
+
+    self.assertEqual(self.environment.getWallSpaces(8+16), [5,6])
+    self.assertEqual(self.environment.getWallSpaces(9+16), [6,7])
+    self.assertEqual(self.environment.getWallSpaces(10+16), [7,8])
+
+    self.assertEqual(self.environment.getWallSpaces(11+16), [5,9])
+    self.assertEqual(self.environment.getWallSpaces(12+16), [6,10])
+    self.assertEqual(self.environment.getWallSpaces(13+16), [7,11])
+    self.assertEqual(self.environment.getWallSpaces(14+16), [8,12])
+
+    self.assertEqual(self.environment.getWallSpaces(15+16), [9,10])
+    self.assertEqual(self.environment.getWallSpaces(16+16), [10,11])
+    self.assertEqual(self.environment.getWallSpaces(17+16), [11,12])
+
+    self.assertEqual(self.environment.getWallSpaces(18+16), [9,13])
+    self.assertEqual(self.environment.getWallSpaces(19+16), [10,14])
+    self.assertEqual(self.environment.getWallSpaces(20+16), [11,15])
+    self.assertEqual(self.environment.getWallSpaces(21+16), [12,16])
+
+    self.assertEqual(self.environment.getWallSpaces(22+16), [13,14])
+    self.assertEqual(self.environment.getWallSpaces(23+16), [14,15])
+    self.assertEqual(self.environment.getWallSpaces(24+16), [15,16])
+
+  def test_listAllAdjacentSpaces(self):
+    self.assertEqual(self.environment.listAllAdjacentSpaces(self.fullWallMap, 3), [3])
+    self.assertEqual(len(self.environment.listAllAdjacentSpaces(self.blankMap, 1)), 16)
+    self.assertEqual(self.environment.listAllAdjacentSpaces(self.endMap, 1), [1, 5, 9, 13, 10, 2, 3, 7, 11, 15, 16, 8, 4])
+    self.assertEqual(self.environment.listAllAdjacentSpaces(self.connectingMap, 1), [1, 2, 3])
+
+  def test_reward(self):
+    self.assertEqual(self.environment.reward(self.fullWallMap, 'W1'), 0)
+    self.assertEqual(self.environment.reward(self.fullWallMap, 'SP1'), 1)
+    self.assertEqual(self.environment.reward(self.endMap, 'W8'), 0)
+    self.assertEqual(self.environment.reward(self.spMap, 'TP2'), 2)
+    self.assertEqual(self.environment.reward(self.spMap, 'TP6'), 3)
+    self.assertEqual(self.environment.reward(self.spMap, 'TP16'), 7)
+    self.assertEqual(self.environment.reward(self.connectingMap, 'W6'), 5)
+
+  def test_curatedActions(self):
+    self.assertEqual('W' not in ', '.join(self.environment.curatedActions(self.blankMap)), True)
+    self.assertEqual('SP' not in ', '.join(self.environment.curatedActions(self.endMap)), True)
+    self.assertEqual('TP' not in ', '.join(self.environment.curatedActions(self.endMap)), True)
+    self.assertEqual('EP' not in ', '.join(self.environment.curatedActions(self.endMap)), True)
+
+  def test_updateState(self):
+    self.assertEqual(self.environment.updateState(self.blankMap, 'SP1'), '1000000000000000000000000000000000000000')
+    self.assertEqual(self.environment.updateState(self.blankMap, 'SP2'), '0100000000000000000000000000000000000000')
+    self.assertEqual(self.environment.updateState(self.blankMap, 'TP3'), '0020000000000000000000000000000000000000')
+    self.assertEqual(self.environment.updateState(self.blankMap, 'EP4'), '0003000000000000000000000000000000000000')
+    self.assertEqual(self.environment.updateState(self.fullWallMap, 'W4'), '0000000000000000444044444444444444444444')
+
+if __name__ == '__main__':
+  unittest.main()
